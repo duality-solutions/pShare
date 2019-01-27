445d3d95db6f2e78ddeac1eff37ebcb075d91175
Chris Sperry <christophermartinsperry@gmail.com>
2019-01-27 22:22:26 +0000

    update merging routines for state persistence

--- src/shared/system/deepMerge.ts
+++ src/shared/system/deepMerge.ts
@@ -1,38 +1,71 @@
-export const deepMerge = <T>(a: T, b: T): T => mergeObjects({} as T, a, b)
+import { blinq } from "blinq";
+import { Enumerable } from "blinq/dist/types/src/Enumerable";
 
-const mergeObjects = <T>(target: T, ...sources: T[]): T => {
-    if (!sources.length) {
-        return target;
-    }
-    const [source, ...remainingSources] = sources;
-    if (source === undefined) {
-        return target;
-    }
+/**
+ * Merges n objects into a single (new) object, or if all objects are equal (===) 
+ * (i.e. they're the same object), then no merge is necessary and that same object is returned.
+ * Objects are assumed to be immutable, allowing maximum reuse. No attempt is made to merge arrays.
+ * For the same property in several items, the last (or rightmost) property's value will be the one
+ * merged into the return value
+ * @param items items to merge. at least 1 item must be provided, else an error will be thrown
+ * if items are mixed mergable/non-mergable (i.e. a mixture of objects with anything else including arrays), 
+ * no attempt will be made to merge. This applies recursively to the entire object graphs being merged.
+ */
+export const deepMerge =
+    <T>(...items: T[]): T => {
+        const itms = blinq(items).groupBy(i => i).select(g => g.key)
+        const numItms = itms.count()
+        let areMergable: boolean
+        try {
+            areMergable = itms.groupBy(isMergeableObject).single().key
 
-    if (isMergebleObject(target) && isMergebleObject(source)) {
-        keys(source).forEach(function (key: keyof T) {
-            if (isMergebleObject(source[key])) {
-                if (!target[key]) {
-                    target[key] = {} as T[keyof T];
-                }
-                mergeObjects(target[key], source[key]);
-            } else {
-                target[key] = source[key];
+        } catch{
+            throw Error("cannot merge a mixed collection of mergeable and non-mergable items")
+        }
+        if (!areMergable) {
+            try {
+                return itms.last()
+            } catch{
+                console.log([...itms])
+                debugger;
+                throw Error("woo");
             }
-        });
+        }
+        switch (numItms) {
+            case 0:
+                throw Error("can't merge 0 items")
+            case 1:
+                return itms.single()
+            default:
+                return entriesToObject(
+                    mergeMergeableChildEntries(
+                        getLatestEntries(itms),
+                        itms))
+        }
     }
+const isObject = (item: any): boolean => item !== null && typeof item === 'object'
 
-    return mergeObjects(target, ...remainingSources);
-};
+const isMergeableObject = (item: any): boolean => isObject(item) && !Array.isArray(item)
 
-const isObject = (item: any): boolean => {
-    return item !== null && typeof item === 'object';
-};
+const entries = <T, TK extends keyof T, TV extends T[TK], TEntry extends [TK, TV]>(o: T): Enumerable<TEntry> => blinq(Object.entries(o) as TEntry[])
 
-const isMergebleObject = (item: any): boolean => {
-    return isObject(item) && !Array.isArray(item);
-};
+const entriesToObject =
+    <T>(entries: Enumerable<[keyof T, T[keyof T]]>): T =>
+        entries.aggregate({} as T, (prev, [k, v]) => { prev[k] = v; return prev })
 
-function keys<O>(o: O) {
-    return Object.keys(o) as (keyof O)[];
-}
+const getLatestEntries = <T, TK extends keyof T, TV extends T[TK], TEntry extends [TK, TV]>(items: Enumerable<T>): Enumerable<TEntry> =>
+    //first() is cheap, but last() is expensive
+    items
+        .reverse() //so it's cheaper to do one reverse() here
+        .selectMany(item => entries<T, TK, TV, TEntry>(item))
+        .groupBy(([k,]) => k)
+        .select(g => g.first()) //than many last() calls here
+
+const mergeMergeableChildEntries = <T, TK extends keyof T, TV extends T[TK]>(entries: Enumerable<[TK, TV]>, items: Enumerable<T>): Enumerable<[TK, TV]> =>
+    entries
+        .select(([propName, value]): [TK, TV] => [propName, mergeObjectPropertyFromItems(propName, items)]);
+
+const mergeObjectPropertyFromItems = <T, TK extends keyof T, TV extends T[TK]>(propName: TK, items: Enumerable<T>): TV =>
+    deepMerge(...items
+        .select(i => i[propName] as TV)
+        .where(x => typeof x !== 'undefined'))
