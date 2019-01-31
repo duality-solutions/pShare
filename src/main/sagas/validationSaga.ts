2fbd765a6cac3c9c758e88914821c500af4dcac3
Chris Sperry <christophermartinsperry@gmail.com>
2019-01-31 02:47:25 +0000

    recursive validation rules

--- src/main/sagas/validationSaga.ts
+++ src/main/sagas/validationSaga.ts
@@ -3,12 +3,14 @@ import { getType } from "typesafe-actions";
 import { Action } from "redux";
 import OnboardingActions from "../../shared/actions/onboarding";
 import delay from "../../shared/system/delay";
+import { blinq } from 'blinq'
+
 export function* validationSaga() {
     yield takeEvery((action: Action<any>) => /^validate\/request\//.test(action.type), function* (action: OnboardingActions) {
         switch (action.type) {
             case getType(OnboardingActions.validateUsername):
                 const username = action.payload
-                const errors: string[] = yield call(validate, username)
+                const errors: string[] = yield call(validate(validationRules), username)
 
                 yield put(OnboardingActions.usernameValidated({ success: errors.length == 0, value: username, errors }));
                 break;
@@ -19,30 +21,42 @@ function isPromise(obj: any) {
     return (typeof obj === 'undefined' ? 'undefined' : typeof (obj)) === 'object' && typeof obj.then === 'function';
 }
 const isAlphaNumeric = (value: string) => /^[A-Za-z0-9]+$/.test(value)
-const isNotEmpty = (value: string) => typeof value !== 'undefined' && value.length > 0
-const failsAfterThreeSeconds = (value: string) => delay(3000).then(() => true)
+const failsAfterThreeSeconds = (value: string) => delay(3000).then(() => false)
 
-const validationRules = [
+interface ValidationTest<T> {
+    test: (value: T) => boolean | Promise<boolean>
+    message: string
+    testsOnSuccess?: ValidationTest<T>[]
+}
+
+const validationRules: ValidationTest<string>[] = [
     {
         test: isAlphaNumeric,
-        message: "value should be alphanumeric"
-    },
-    {
-        test: isNotEmpty,
-        message: "value should not be empty"
-    }, {
-        test: failsAfterThreeSeconds,
-        message: "oh no. 3s failure"
+        message: "value should be at least 1 alphanumeric",
+        testsOnSuccess: [{
+            test: failsAfterThreeSeconds,
+            message: "oh no. 3s failure"
+        }]
     }
+
 ]
 
-const validate = async (value: string) => {
-    const resolvedTests = await Promise.all(
-        validationRules.map(async (rule) => {
-            const testResult = rule.test(value)
-            const result = (isPromise(testResult) ? await testResult : testResult) as boolean
-            return { result, message: rule.message }
-        }))
-    return resolvedTests.filter(test => !test.result).map(test => test.message)
-
+interface ValidationTestResult {
+    result: boolean
+    message: string
 }
+
+const runTests = <T>(rules: ValidationTest<T>[]) => async (value: T): Promise<ValidationTestResult[]> => {
+    const results = await Promise.all(blinq(rules).select(async (rule) => {
+        const testResult = rule.test(value)
+        const result = (isPromise(testResult) ? await testResult : testResult) as boolean
+
+        if (result && typeof rule.testsOnSuccess !== 'undefined') {
+            return await runTests(rule.testsOnSuccess)(value)
+        }
+        return [{ result, message: rule.message }]
+    }))
+    return blinq(results).selectMany(x => x).toArray()
+}
+
+const validate = <T>(rules: ValidationTest<T>[]) => async (value: T) => blinq(await runTests(rules)(value)).where(r => !r.result).select(r => r.message).toArray()
