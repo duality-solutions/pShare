8c5ad58ced1331117c05e73f470d1369a8f7fe37
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-01 12:57:55 +0000

    validated actions now contain an isError field

--- src/main/sagas/validationSaga.ts
+++ src/main/sagas/validationSaga.ts
@@ -4,37 +4,23 @@ import OnboardingActions from "../../shared/actions/onboarding";
 import { validateDisplayname } from "../validation/validateDisplayname";
 import { validateToken } from "../validation/validateToken";
 import { validateUsername } from "../validation/validateUsername";
+import { ValidationResult } from "../../shared/system/validator/ValidationResult";
 
 export function* validationSaga() {
     yield takeEvery(getType(OnboardingActions.validateUsername), function* (action: ActionType<typeof OnboardingActions.validateUsername>) {
         const username = action.payload
-        const validationMessages: string[] = yield call(validateUsername, username)
-        yield put(OnboardingActions.usernameValidated({
-            success: validationMessages.length == 0,
-            value: username,
-            validationMessages,
-            isError: false
-        }));
+        const validationResult: ValidationResult<string> = yield call(validateUsername, username)
+        yield put(OnboardingActions.usernameValidated(validationResult));
     })
     yield takeEvery(getType(OnboardingActions.validateDisplayname), function* (action: ActionType<typeof OnboardingActions.validateDisplayname>) {
         const displayname = action.payload
-        const validationMessages: string[] = yield call(validateDisplayname, displayname)
-        yield put(OnboardingActions.displaynameValidated({
-            success: validationMessages.length == 0,
-            value: displayname,
-            validationMessages,
-            isError: false
-        }));
+        const validationResult: ValidationResult<string> = yield call(validateDisplayname, displayname)
+        yield put(OnboardingActions.displaynameValidated(validationResult));
     })
 
     yield takeEvery(getType(OnboardingActions.validateToken), function* (action: ActionType<typeof OnboardingActions.validateToken>) {
         const token = action.payload
-        const validationMessages: string[] = yield call(validateToken, token)
-        yield put(OnboardingActions.tokenValidated({
-            success: validationMessages.length == 0,
-            value: token,
-            validationMessages,
-            isError: false
-        }))
+        const validationResult: ValidationResult<string> = yield call(validateToken, token)
+        yield put(OnboardingActions.tokenValidated(validationResult))
     })
 }
--- src/shared/system/validator/validate.ts
+++ src/shared/system/validator/validate.ts
@@ -1,11 +1,23 @@
 import { blinq } from 'blinq';
 import { isPromise } from "../isPromise";
 import { ValidationTest } from './ValidationTest';
+import { ValidationResult } from './ValidationResult';
 
 export const validate =
     <T>(rules: ValidationTest<T>[]) =>
-        async (value: T) =>
-            blinq(await runTests(rules, value)).where(r => !r.result).select(r => r.message).toArray();
+        async (value: T) => {
+            const testResults = await runTests(rules, value);
+            const failedTestResults = blinq(testResults).where(r => !r.result);
+            const failedValidationMessages = failedTestResults.select(r => r.message).toArray();
+            const isError = failedTestResults.any(t => t.isError)
+            const result: ValidationResult<T> = {
+                isError,
+                success: !failedTestResults.any(),
+                validationMessages: failedValidationMessages,
+                value
+            }
+            return result
+        };
 
 interface ValidationTestResult {
     result: boolean
