5176a6e38a8a1e2d48e5710d0814527268c0db2b
tarun genwa <tarungenwa@taruns-MacBook-Pro.local>
2019-02-01 15:19:23 +0530

    reduced delay

--- src/main/validation/tokenValidationRules.ts
+++ src/main/validation/tokenValidationRules.ts
@@ -2,7 +2,7 @@ import delay from "../../shared/system/delay";
 import { ValidationTest } from "../../shared/system/validator/ValidationTest";
 
 const isValidToken = (value: string) => /^[A-Za-z0-9]{6,}$/.test(value);
-const mockDynamicdCall = (value: string) => delay(5000).then(() => value !== "fail");
+const mockDynamicdCall = (value: string) => delay(2000).then(() => value !== "fail");
 
 const tokenValidationRules: ValidationTest<string>[] = [
     {
