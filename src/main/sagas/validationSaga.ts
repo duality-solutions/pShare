1c60b78fa4300b4bea1badb1ec2d012fab5bc845
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-01 12:33:31 +0000

    remove regex based tests on validation action types and make validation saga look like onboarding saga (separate takeEvery for each validation type)

--- src/main/sagas/validationSaga.ts
+++ src/main/sagas/validationSaga.ts
@@ -1,33 +1,25 @@
-import { Action } from "redux";
 import { call, put, takeEvery } from "redux-saga/effects";
-import { getType } from "typesafe-actions";
+import { getType, ActionType } from "typesafe-actions";
 import OnboardingActions from "../../shared/actions/onboarding";
 import { validateDisplayname } from "../validation/validateDisplayname";
 import { validateToken } from "../validation/validateToken";
 import { validateUsername } from "../validation/validateUsername";
 
 export function* validationSaga() {
-    yield takeEvery((action: Action<any>) => /^validate\/request\//.test(action.type), function* (action: OnboardingActions) {
-        switch (action.type) {
-            case getType(OnboardingActions.validateUsername): {
-                const username = action.payload
-                const errors: string[] = yield call(validateUsername, username)
+    yield takeEvery(getType(OnboardingActions.validateUsername), function* (action: ActionType<typeof OnboardingActions.validateUsername>) {
+        const username = action.payload
+        const errors: string[] = yield call(validateUsername, username)
+        yield put(OnboardingActions.usernameValidated({ success: errors.length == 0, value: username, errors }));
+    })
+    yield takeEvery(getType(OnboardingActions.validateDisplayname), function* (action: ActionType<typeof OnboardingActions.validateDisplayname>) {
+        const displayname = action.payload
+        const errors: string[] = yield call(validateDisplayname, displayname)
+        yield put(OnboardingActions.displaynameValidated({ success: errors.length == 0, value: displayname, errors }));
+    })
 
-                yield put(OnboardingActions.usernameValidated({ success: errors.length == 0, value: username, errors }));
-                break;
-            }
-            case getType(OnboardingActions.validateDisplayname): {
-                const displayname = action.payload
-                const errors: string[] = yield call(validateDisplayname, displayname)
-                yield put(OnboardingActions.displaynameValidated({ success: errors.length == 0, value: displayname, errors }));
-                break;
-            }
-            case getType(OnboardingActions.validateToken): {
-                const token = action.payload
-                const errors: string[] = yield call(validateToken, token)
-                yield put(OnboardingActions.tokenValidated({ success: errors.length == 0 , value: token, errors }))
-            }
-        }
-    });
+    yield takeEvery(getType(OnboardingActions.validateToken), function* (action: ActionType<typeof OnboardingActions.validateToken>) {
+        const token = action.payload
+        const errors: string[] = yield call(validateToken, token)
+        yield put(OnboardingActions.tokenValidated({ success: errors.length == 0, value: token, errors }))
+    })
 }
-
