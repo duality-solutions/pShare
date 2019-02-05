d7a1d6a8780d56e51cd6e8dae71f89048072eaff
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-05 16:11:38 +0000

    simplify payload type annotation

--- src/main/sagas/onboardingSaga.ts
+++ src/main/sagas/onboardingSaga.ts
@@ -1,11 +1,10 @@
 import { put, take, takeEvery } from "redux-saga/effects";
-import { getType } from "typesafe-actions";
+import { getType, ActionType } from "typesafe-actions";
 import OnboardingActions from "../../shared/actions/onboarding";
 export function* onboardingSaga() {
     yield takeEvery(getType(OnboardingActions.submitUserName), function* (action: ReturnType<typeof OnboardingActions.submitUserName>) {
         yield put(OnboardingActions.validateUserName(action.payload));
-        //I'm sure there's a better way to get this type than using returntype
-        const { payload: validationResult }: ReturnType<typeof OnboardingActions.userNameValidated> = yield take(getType(OnboardingActions.userNameValidated));
+        const { payload: validationResult }: ActionType<typeof OnboardingActions.userNameValidated> = yield take(getType(OnboardingActions.userNameValidated));
         console.log("validation result is ", validationResult);
         if (validationResult.success) {
             yield put(OnboardingActions.enterUserName())
@@ -13,8 +12,7 @@ export function* onboardingSaga() {
     });
     yield takeEvery(getType(OnboardingActions.submitCommonName), function* (action: ReturnType<typeof OnboardingActions.submitCommonName>) {
         yield put(OnboardingActions.validateCommonName(action.payload));
-        //I'm sure there's a better way to get this type than using returntype
-        const { payload: validationResult }: ReturnType<typeof OnboardingActions.commonNameValidated> = yield take(getType(OnboardingActions.commonNameValidated));
+        const { payload: validationResult }: ActionType<typeof OnboardingActions.commonNameValidated> = yield take(getType(OnboardingActions.commonNameValidated));
         console.log("validation result is ", validationResult);
         if (validationResult.success) {
             yield put(OnboardingActions.enterCommonName())
@@ -23,8 +21,7 @@ export function* onboardingSaga() {
 
     yield takeEvery(getType(OnboardingActions.submitToken), function* (action: ReturnType<typeof OnboardingActions.submitToken>) {
         yield put(OnboardingActions.validateToken(action.payload))
-        //I'm sure there's a better way to get this type than using returntype
-        const { payload: validationResult }: ReturnType<typeof OnboardingActions.tokenValidated> = yield take(getType(OnboardingActions.tokenValidated))
+        const { payload: validationResult }: ActionType<typeof OnboardingActions.tokenValidated> = yield take(getType(OnboardingActions.tokenValidated))
         console.log("validation result is ", validationResult)
         if(validationResult.success) {
             yield put(OnboardingActions.enterToken())
