e0cc0a7cb6a20ddf8ef7ed074e87f63a5565ea14
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-06 02:55:07 +0000

    simplify navsaga

--- src/renderer/sagas/navSaga.ts
+++ src/renderer/sagas/navSaga.ts
@@ -1,9 +1,10 @@
 import { push } from "connected-react-router";
-import { put, select, take, takeLatest } from "redux-saga/effects";
+import { put, select, take, takeLatest, cancel } from "redux-saga/effects";
 import { getType } from "typesafe-actions";
 import { RendererRootState } from "../reducers";
 import RootActions from "./../../shared/actions";
 import { ActionCreator } from "typesafe-actions/dist/types";
+import { Task } from "redux-saga";
 
 //const delay = (time: number) => new Promise(r => setTimeout(r, time));
 
@@ -44,22 +45,37 @@ export function* navSaga() {
         console.log("nav saga: navigating to Onboarding -- /CreateAccount")
         yield put(push("/CreateAccount"))
         console.log("nav saga navigating to /CreateAccount")
+
+        const navMap = new Map<string, [string, boolean]>()
+        const registerNavAction = <T extends string>(
+            action: ActionCreator<T>,
+            route: string,
+            shouldCancel: boolean = false
+        ) =>
+            navMap.set(getType(action), [route, shouldCancel])
+
+        registerNavAction(RootActions.createAccount, "/EnterUserName")
+        registerNavAction(RootActions.enterUserName, "/EnterCommonName")
+        registerNavAction(RootActions.enterCommonName, "/EnterToken")
+        registerNavAction(RootActions.enterToken, "/CreatingBdapAccount")
+        registerNavAction(RootActions.resetOnboarding, "/EnterUserName")
+        registerNavAction(RootActions.enterCreatingBdapAccount, "/Main", true)
+
+
+        const bdapAccountTask: Task =
+            yield takeLatest((action: RootActions) => navMap.has(action.type), function* (action: RootActions) {
+                const navTarget = navMap.get(action.type)
+                if (typeof navTarget !== 'undefined') {
+                    const [route, shouldCancel] = navTarget
+                    yield put(push(route))
+                    if (shouldCancel) {
+                        yield cancel(bdapAccountTask)
+                    }
+                }
+            })
+
     }
 
-    const navMap = new Map<string, string>()
-    const registerNavAction = <T extends string>(action: ActionCreator<T>, route: string) => navMap.set(getType(action), route)
-    registerNavAction(RootActions.createAccount, "/EnterUserName")
-    registerNavAction(RootActions.enterUserName, "/EnterCommonName")
-    registerNavAction(RootActions.enterCommonName, "/EnterToken")
-    registerNavAction(RootActions.enterToken, "/CreatingBdapAccount")
-    registerNavAction(RootActions.enterCreatingBdapAccount, "/Main")
-    registerNavAction(RootActions.resetOnboarding, "/EnterUserName")
 
-    yield takeLatest((action: RootActions) => navMap.has(action.type), function* (action: RootActions) {
-        const navTarget = navMap.get(action.type)
-        if (typeof navTarget !== 'undefined') {
-            yield put(push(navTarget))
-        }
-    })
 
 }
