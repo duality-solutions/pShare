f6c2ca1d25d42f17fc4521e91f655c270fe60289
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-06 00:40:56 +0000

    more post-merge cleanup

--- src/renderer/sagas/beginCreateBdapAccountSaga.ts
+++ src/renderer/sagas/beginCreateBdapAccountSaga.ts
@@ -2,12 +2,12 @@ import { put, select, takeEvery } from "redux-saga/effects";
 import { getType } from "typesafe-actions";
 import { RendererRootState } from "../reducers";
 import OnboardingActions from "../../shared/actions/onboarding";
-import { OnboardingBdapAccountOptionsValidationState } from "../reducers/onboarding";
+import { OnboardingBdapAccountOptionsValidationState } from "../reducers/bdapAccountFormValues";
 
 
 export function* beginCreateBdapAccountSaga() {
     yield takeEvery(getType(OnboardingActions.beginCreateBdapAccount), function* (_) {
-        const onboardingValidationState: OnboardingBdapAccountOptionsValidationState = yield select((state: RendererRootState) => state.onboarding)
+        const onboardingValidationState: OnboardingBdapAccountOptionsValidationState = yield select((state: RendererRootState) => state.bdapAccountFormValues)
         if (!onboardingValidationState.isValid) {
             throw Error("unexpected value, state.onboarding.isValid is false")
         }
