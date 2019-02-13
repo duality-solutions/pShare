a7ed1380145b8198e31196edb0e184a135954ac4
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-13 21:12:59 +0000

    add optional opaque prop to loading spinner

--- src/renderer/components/ui-elements/LoadingSpinner.tsx
+++ src/renderer/components/ui-elements/LoadingSpinner.tsx
@@ -4,10 +4,10 @@ import Box from './Box';
 import Container from './Container';
 import Text from './Text';
 interface StyledPshareSpinnerProps {
-    width?: number,
+        width?: number,
 }
 
-const StyledOverlay = styled('div')`
+const StyledOverlay = styled('div') <{ opaque?: boolean }>`
   position: fixed;
   z-index: 999;
   height: 100%;
@@ -18,12 +18,12 @@ const StyledOverlay = styled('div')`
   left: 0;
   bottom: 0;
   right: 0;
-  background-color: rgba(255,255,255,0.8);
+  background-color: rgba(255,255,255,${props => props.opaque ? 1 : 0.8});
 `
 
-const StyledPshareSpinner = styled('div')<StyledPshareSpinnerProps>`
+const StyledPshareSpinner = styled('div') <StyledPshareSpinnerProps>`
     width: ${props => `${props.width}px` || '100px'};
-    height: ${props => props.width ? `${props.width * (239/167)}px` : '100px' };
+    height: ${props => props.width ? `${props.width * (239 / 167)}px` : '100px'};
     margin: auto;
 `;
 
@@ -92,30 +92,30 @@ const StyledPolygon3 = styled('polygon')`
         opacity: 0.05;
 `
 
-const LoadingSpinner:React.FunctionComponent<{ active?: boolean, label?: string, size?:number }>= ({ active, label, size }) => 
-    <> {active && 
-    <StyledOverlay>
-        <Container margin="25% 0 auto 0">
-        <StyledPshareSpinner width={size || 100}>
-        <StyledPshareSpinnerContainer>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 167 239" preserveAspectRatio="xMidYMid meet">
-        <StyledPath1  d="M 83,0 0,48 v 96 c 83,47 0,0 83,48 l 83,-48 V 48 Z M 123,119 83,142 43,119 V 73 L 83,50 123,73 Z"/>
-        <StyledPolygon1 points="249,179 209,202 209,249 249,272 289,249 289,202 " transform="translate(-166,-129)" />
-        <StyledPath2  d="M 0,238 43,213 V 167 L 0,143 Z" />
-        <StyledPolygon2 points="250,226 333,177 333,274 250,322 " transform="translate(-166,-129)"/>
-        <StyledPolygon3 points="250,129 333,177 250,226 166,177 " transform="translate(-166,-129)"/>
-        </svg>
-        </StyledPshareSpinnerContainer>
-        </StyledPshareSpinner>
-        <Box display="flex" width="100%" align="center" direction="row">
-        <Box display="flex" direction="column" background="#f2f2f2" width="0" minWidth="30%"  borderRadius="4px" padding="0.5em">
-        <Text align="center" margin="0">
-        {label}
-        </Text>
-        </Box>
-        </Box>
-        </Container>
-    </StyledOverlay>} 
-    </>
+const LoadingSpinner: React.FunctionComponent<{ active?: boolean, label?: string, size?: number, opaque?: boolean }> = ({ active, label, size, opaque }) =>
+        <> {active &&
+                <StyledOverlay opaque={opaque}>
+                        <Container margin="25% 0 auto 0">
+                                <StyledPshareSpinner width={size || 100}>
+                                        <StyledPshareSpinnerContainer>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 167 239" preserveAspectRatio="xMidYMid meet">
+                                                        <StyledPath1 d="M 83,0 0,48 v 96 c 83,47 0,0 83,48 l 83,-48 V 48 Z M 123,119 83,142 43,119 V 73 L 83,50 123,73 Z" />
+                                                        <StyledPolygon1 points="249,179 209,202 209,249 249,272 289,249 289,202 " transform="translate(-166,-129)" />
+                                                        <StyledPath2 d="M 0,238 43,213 V 167 L 0,143 Z" />
+                                                        <StyledPolygon2 points="250,226 333,177 333,274 250,322 " transform="translate(-166,-129)" />
+                                                        <StyledPolygon3 points="250,129 333,177 250,226 166,177 " transform="translate(-166,-129)" />
+                                                </svg>
+                                        </StyledPshareSpinnerContainer>
+                                </StyledPshareSpinner>
+                                <Box display="flex" width="100%" align="center" direction="row">
+                                        <Box display="flex" direction="column" background="#f2f2f2" width="0" minWidth="30%" borderRadius="4px" padding="0.5em">
+                                                <Text align="center" margin="0">
+                                                        {label}
+                                                </Text>
+                                        </Box>
+                                </Box>
+                        </Container>
+                </StyledOverlay>}
+        </>
 
 export default LoadingSpinner
