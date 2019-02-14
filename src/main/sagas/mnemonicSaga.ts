68e5ba83e24a67acb014cfdee6b88efdfeceb84e
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-14 00:24:09 +0000

    implement mnemonicSaga

--- src/main/sagas/mnemonicSaga.ts
+++ src/main/sagas/mnemonicSaga.ts
@@ -1,6 +1,21 @@
-import { delay } from "redux-saga";
+import { takeEvery, select, put } from "redux-saga/effects";
+import { getType } from "typesafe-actions";
+import { OnboardingActions } from "../../shared/actions/onboarding";
+import { MainRootState } from "../reducers";
+import { RpcCommandFunc } from "./effects/helpers/RpcCommandFunc";
+import { unlockedCommandEffect } from "./effects/unlockedCommandEffect";
+import { HdInfo } from "../../dynamicdInterfaces/HdInfo";
 
 export function* mnemonicSaga() {
-    yield delay(5000)
+    yield takeEvery(getType(OnboardingActions.mnemonicWarningAccepted), function* () {
+        const walletPassword = yield select((state: MainRootState) => state.user.sessionWalletPassword)
+
+        const mnemonic: string = yield unlockedCommandEffect(walletPassword, async (command: RpcCommandFunc) => {
+            const hdInfo: HdInfo = await command("dumphdinfo");
+            return hdInfo.mnemonic;
+        })
+
+        yield put(OnboardingActions.mnemonicAcquired(mnemonic))
+    })
 }
 
