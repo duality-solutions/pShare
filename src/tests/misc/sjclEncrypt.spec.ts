67d0dd50bdf4a1be409d6f0785d45aa5238a2e9a
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-14 14:15:58 +0000

    encryption lib installed and testing correctly

--- src/tests/misc/sjclEncrypt.spec.ts
+++ src/tests/misc/sjclEncrypt.spec.ts
@@ -1,10 +1,15 @@
-import { encrypt, decrypt } from "sjcl"
+import { encrypt, decrypt } from "sjcl-local"
 
 test("can encrypt", () => {
     const password = "monkey";
     const payload = "this is a test";
-    const encrypted = encrypt(password, payload)
+    const encrypted = encrypt(password, payload,{iter:100000,ks:256,ts:128})
+    expect(typeof encrypted).toBe("string")
+    console.log(`encrypted data : ${encrypted}`)
     const decrypted = decrypt(password, encrypted)
+    expect(payload).not.toBe(encrypted)
     expect(payload).toBe(decrypted)
+
+    expect(()=>decrypt("foo",encrypted)).toThrow()
 
 })
