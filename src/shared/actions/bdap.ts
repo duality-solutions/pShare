057e9a565fc030f5aabc3b425233858c1bb94811
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-21 16:28:46 +0000

    add actions for handling bdap links/users

--- src/shared/actions/bdap.ts
+++ src/shared/actions/bdap.ts
@@ -1,9 +1,18 @@
 import { ActionType, createStandardAction } from 'typesafe-actions';
+import { GetUsersEntry } from '../../dynamicdInterfaces/getusers/GetUsersEntry';
+import { LinkRequestEntry } from '../../dynamicdInterfaces/links/LinkRequestEntry';
+
 export const BdapActions = {
 
-    someAction: createStandardAction('bdap/SOME_ACTION')<void>(),
+    getUsers: createStandardAction('bdap/GET_USERS')<void>(),
+    getUsersSuccess: createStandardAction('bdap/GET_USERS_SUCCESS')<GetUsersEntry[]>(),
 
- 
+    getCompleteLinks: createStandardAction('bdap/GET_COMPLETE_LINKS')<void>(),
+    getCompleteLinksSuccess: createStandardAction('bdap/GET_COMPLETE_LINKS_SUCCESS')<LinkRequestEntry[]>(),
+
+
+    getPendingLinks: createStandardAction('bdap/GET_PENDING_LINKS')<void>(),
+    getPendingLinksSuccess: createStandardAction('bdap/GET_PENDING_LINKS_SUCCESS')<LinkRequestEntry[]>(),
 
 }
 
