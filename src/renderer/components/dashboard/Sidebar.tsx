1cae6ec00e372bccd3494d649b1450a99f34378f
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-21 13:55:00 +0000

    rework Sidebar.ts to map an array of TabInfo for each tab

--- src/renderer/components/dashboard/Sidebar.tsx
+++ src/renderer/components/dashboard/Sidebar.tsx
@@ -4,34 +4,65 @@ import { PlainAppLogo, MyLinksIcon, InboxIcon, OutboxIcon, InvitesIcon } from ".
 import Text from "../ui-elements/Text";
 import { RouteComponentProps } from "react-router";
 
-
 export const Sidebar: FunctionComponent<RouteComponentProps<any>> = ({ history, location }) => <>
     <UL>
         <div style={{ borderBottom: "solid 0.1px #d2d2d2 " }}><PlainAppLogo /></div>
-        <LI onClick={() => history.push('/Dashboard/MyLinks')}
-            dark={location.pathname === '/Dashboard/MyLinks' ||
-                location.pathname === '/Dashboard/AddLinks'}>
-            <MyLinksIcon white={location.pathname === '/Dashboard/MyLinks' ||
-                location.pathname === '/Dashboard/AddLinks'}
-                width="36px" height="36px" margin="0 0 0 1em" />
-            <Text color={(location.pathname === '/Dashboard/MyLinks' || location.pathname === '/Dashboard/AddLinks') ? "white" : "#4a4a4a"}
-                margin="0" align="center" fontSize="0.6em" fontWeight="bold">My Links</Text>
-        </LI>
-        <LI disabled>
-            <InboxIcon width="36px" height="36px" margin="0 0 0 0.9em" />
-            <Text color="#4a4a4a" margin="0" align="center" fontSize="0.6em" fontWeight="bold">Inbox</Text>
-        </LI>
-        <LI disabled>
-            <OutboxIcon width="36px" height="36px" margin="0 0 0 0.9em" />
-            <Text color="#4a4a4a" margin="0" align="center" fontSize="0.6em" fontWeight="bold">Outbox</Text>
-        </LI>
-        <LI onClick={() => history.push('/Dashboard/Invites')}
-            dark={location.pathname === '/Dashboard/Invites'}>
-            <InvitesIcon white={location.pathname === '/Dashboard/Invites'}
-                width="36px" height="36px" margin="0 0 0 0.9em" />
-            <Text color={location.pathname === '/Dashboard/Invites' ? "white" : "#4a4a4a"}
-                margin="0" align="center" fontSize="0.6em" fontWeight="bold">Invites</Text>
-        </LI>
+        {
+            tabs.map((t, idx) =>
+                <LI key={idx} disabled={t.disabled}
+                    onClick={t.disabled ? undefined : () => history.push(t.location)}
+                    dark={t.isSelected(location.pathname)}>
+
+                    {t.icon(t.isSelected(location.pathname))}
+
+                    <Text
+                        color={t.isSelected(location.pathname) ? "white" : "#4a4a4a"}
+                        margin="0"
+                        align="center"
+                        fontSize="0.6em"
+                        fontWeight="bold">{
+                            t.text
+                        }
+                    </Text>
+                </LI>)
+        }
     </UL>
 </>
 
+interface TabInfo {
+    location: string
+    icon: (selected: boolean) => any
+    text: string
+    isSelected: (pathname: string) => boolean,
+    disabled?: boolean
+}
+const tabs: TabInfo[] = [
+    {
+        location: '/Dashboard/MyLinks',
+        icon: (selected: boolean) => <MyLinksIcon white={selected}
+            width="36px" height="36px" margin="0 0 0 1em" />,
+        text: "My Links",
+        isSelected: (pathname: string) => pathname === '/Dashboard/MyLinks' || pathname === '/Dashboard/AddLinks'
+    },
+    {
+        location: '/Dashboard/Inbox',
+        icon: (selected: boolean) => <InboxIcon width="36px" height="36px" margin="0 0 0 0.9em" />,
+        text: "Inbox",
+        isSelected: (pathname: string) => pathname === '/Dashboard/Inbox',
+        disabled: true
+    },
+    {
+        location: '/Dashboard/Outbox',
+        icon: (selected: boolean) => <OutboxIcon width="36px" height="36px" margin="0 0 0 0.9em" />,
+        text: "Inbox",
+        isSelected: (pathname: string) => pathname === '/Dashboard/Outbox',
+        disabled: true
+    },
+    {
+        location: '/Dashboard/Invites',
+        icon: (selected: boolean) => <InvitesIcon white={selected} width="36px" height="36px" margin="0 0 0 0.9em" />,
+        text: "Inbox",
+        isSelected: (pathname: string) => pathname === '/Dashboard/Invites',
+    },
+
+]
