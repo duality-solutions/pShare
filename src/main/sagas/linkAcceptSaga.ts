c64f8f7766597052f2e9aa98ac847a3efbb4fccb
Chris Sperry <christophermartinsperry@gmail.com>
2019-03-08 01:40:56 +0000

    link accept wired-in (untested)

--- src/main/sagas/linkAcceptSaga.ts
+++ src/main/sagas/linkAcceptSaga.ts
@@ -1,10 +1,17 @@
-import { takeEvery, select } from "redux-saga/effects";
+import { takeEvery, select, put } from "redux-saga/effects";
 import { getType, ActionType } from "typesafe-actions";
 import { BdapActions } from "../../shared/actions/bdap";
 import { MainRootState } from "../reducers";
 import { LinkAcceptResponse } from "../../dynamicdInterfaces/LinkAcceptResponse";
 import { unlockedCommandEffect } from "./effects/unlockedCommandEffect";
 export function* linkAcceptSaga() {
+
+
+    yield takeEvery(getType(BdapActions.beginAcceptLink), function* ({ payload: { recipient, requestor } }: ActionType<typeof BdapActions.beginAcceptLink>) {
+        const action = BdapActions.acceptLink({ recipient, requestor })
+        yield put(action)
+    })
+
     yield takeEvery(getType(BdapActions.acceptLink), function* (action: ActionType<typeof BdapActions.acceptLink>) {
         const { payload: { recipient, requestor, registrationDays } } = action;
         const userName: string = yield select((state: MainRootState) => state.user.userName);
@@ -16,11 +23,19 @@ export function* linkAcceptSaga() {
         }
 
 
-        // @ts-ignore
-        const response: LinkAcceptResponse =
-            yield unlockedCommandEffect(command =>
-                typeof registrationDays === 'undefined'
-                    ? command("link", "accept", requestor, recipient)
-                    : command("link", "accept", requestor, recipient, registrationDays));
+        let response: LinkAcceptResponse;
+        try {
+            response =
+                yield unlockedCommandEffect(command =>
+                    typeof registrationDays === 'undefined'
+                        ? command("link", "accept", requestor, recipient)
+                        : command("link", "accept", requestor, recipient, registrationDays.toString()));
+        } catch (err) {
+            debugger
+            throw err
+        }
+        console.log(response)
+        yield put(BdapActions.getCompleteLinks())
+        yield put(BdapActions.getPendingAcceptLinks())
     });
 }
