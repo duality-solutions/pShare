fee6824cfc838f649e9766e4d2b9dda8e9636958
Chris Sperry <christophermartinsperry@gmail.com>
2019-05-02 14:54:53 +0100

    refresh pshare-filelist every 10s when in shared files UI, stop when we naviagte away

--- src/main/sagas/startViewSharedFilesSaga.ts
+++ src/main/sagas/startViewSharedFilesSaga.ts
@@ -1,4 +1,4 @@
-import { takeEvery, call, put, select } from "redux-saga/effects";
+import { takeEvery, call, put, select, actionChannel, flush, race } from "redux-saga/effects";
 import { getType, ActionType } from "typesafe-actions";
 import { DashboardActions } from "../../shared/actions/dashboard";
 import { RpcClient } from "../RpcClient";
@@ -7,6 +7,9 @@ import { unlockedCommandEffect } from "./effects/unlockedCommandEffect";
 import { MainRootState } from "../reducers";
 import { FileListActions } from "../../shared/actions/fileList";
 import { PublicSharedFile } from "../../shared/types/PublicSharedFile";
+import { SharedFilesActions } from "../../shared/actions/sharedFiles";
+import { take } from "rxjs/operators";
+import { delay } from "redux-saga";
 interface BdapLinkData {
     link_requestor: string,
     link_acceptor: string,
@@ -23,23 +26,57 @@ interface BdapLinkData {
 export function* startViewSharedFilesSaga(rpcClient: RpcClient) {
     yield takeEvery(getType(DashboardActions.startViewSharedFiles), function* (action: ActionType<typeof DashboardActions.startViewSharedFiles>) {
         const linkedUserName = action.payload
-        const userInfo: GetUserInfo = yield call(() => rpcClient.command("getuserinfo", linkedUserName))
+        const linkedUserInfo: GetUserInfo = yield call(() => rpcClient.command("getuserinfo", linkedUserName))
         const userName: string = yield select((s: MainRootState) => s.user.userName)
-        let linkData: BdapLinkData | undefined
-        try {
-            linkData = yield unlockedCommandEffect(rpcClient, command => command("getbdaplinkdata", userInfo.object_id, userName, "pshare-filelist"))
-        } catch (err) {
-            if (!/getbdaplinkdata: ERRCODE: 5626 - Failed to get record:/.test(err.message)) {
-                throw err
-            }
+
+        const chan = yield actionChannel(getType(SharedFilesActions.close))
+        const checkClosed = function* () {
+            const closeActions: any[] = yield flush(chan)
+            return closeActions.some(x => true)
+        }
+        const linkData: BdapLinkData | undefined = yield call(() => readAndDispatchFileList(rpcClient, linkedUserInfo, userName))
+        if (yield* checkClosed()) {
+            return
         }
         if (!linkData) {
             yield put(FileListActions.fileListFetchFailed())
         }
         else {
-            const data:PublicSharedFile[] = JSON.parse(linkData.get_value);
+            const data: PublicSharedFile[] = JSON.parse(linkData.get_value);
             yield put(FileListActions.fileListFetchSuccess(data))
         }
-        yield put(DashboardActions.viewSharedFiles(userInfo))
+
+        yield put(DashboardActions.viewSharedFiles(linkedUserInfo))
+        for (; ;) {
+            yield delay(10000)
+            if (yield* checkClosed()) {
+                break
+            }
+            const linkData: BdapLinkData | undefined = yield call(() => readAndDispatchFileList(rpcClient, linkedUserInfo, userName))
+            if (yield* checkClosed()) {
+                break
+            }
+            if (!linkData) {
+                yield put(FileListActions.fileListFetchFailed())
+            }
+            else {
+                const data: PublicSharedFile[] = JSON.parse(linkData.get_value);
+                yield put(FileListActions.fileListFetchSuccess(data))
+            }
+
+
+        }
     })
+}
+
+function* readAndDispatchFileList(rpcClient: RpcClient, linkedUserInfo: GetUserInfo, userName: string) {
+    let linkData: BdapLinkData | undefined
+    try {
+        linkData = yield unlockedCommandEffect(rpcClient, command => command("getbdaplinkdata", linkedUserInfo.object_id, userName, "pshare-filelist"))
+    } catch (err) {
+        if (!/getbdaplinkdata: ERRCODE: 5626 - Failed to get record:/.test(err.message)) {
+            throw err
+        }
+    }
+    return linkData
 }
