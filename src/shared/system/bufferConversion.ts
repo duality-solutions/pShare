4afe45daa45440c5ad6a2669f3f2d746922396a5
Chris Sperry <christophermartinsperry@gmail.com>
2019-05-10 15:26:38 +0100

    harden parameter checking in bufferConversion

--- src/shared/system/bufferConversion.ts
+++ src/shared/system/bufferConversion.ts
@@ -1,11 +1,14 @@
 export function toBuffer(ab: ArrayBuffer, offset: number, amt: number) {
-    const off = offset == null ? 0 : Math.min(offset, ab.byteLength)
-    const len = Math.min(typeof amt !== 'undefined' ? amt : ab.byteLength - off, ab.byteLength);
-    return Buffer.from(ab, off, len)
+    if (offset > ab.byteLength || offset < 0 || amt > ab.byteLength - offset || amt < 0) {
+        throw Error("out of range parameters")
+    }
+    return Buffer.from(ab, offset, amt)
 }
 export function toArrayBuffer(buf: Buffer, offset: number, amt: number) {
-    const off = offset == null ? 0 : Math.min(offset, buf.length)
-    const len = Math.min(amt != null ? amt : buf.length - off, buf.length);
-    const ab = buf.buffer.slice(off, len);
+
+    if (offset > buf.length || offset < 0 || amt > buf.length - offset || amt < 0) {
+        throw Error("out of range parameters")
+    }
+    const ab = buf.buffer.slice(offset, amt);
     return ab;
 }
