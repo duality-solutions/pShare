f6e3e71abd2f1982d4746678aa59aa2c2bf96eb5
TarunGenwa <tarungenwa@gmail.com>
2019-05-27 22:24:25 +0530

    file deleteion complete with modal

--- src/renderer/components/ui-elements/Image.tsx
+++ src/renderer/components/ui-elements/Image.tsx
@@ -16,7 +16,9 @@ import requestsent from "../../assets/svgs/request-sent-32.svg";
 import close from "../../assets/svgs/close-32.svg";
 import viewbtn from "../../assets/svgs/viewbtn.svg";
 import doc from "../../assets/svgs/p-share-doc.svg";
-
+import deleteicon from "../../assets/delete.svg";
+import checkicon from "../../assets/check.svg";
+import cancelicon from "../../assets/cancel.svg";
 
 interface ImageProps {
     src?: string,
@@ -80,7 +82,17 @@ const ViewBtnIcon: React.FunctionComponent<ImageProps> =
 const DocumentSvg: React.FunctionComponent<ImageProps> =
     ({ width, height, margin, onClick }) => <SvgIcon onClick={onClick} src={doc} width={width || '50px'} height={height || "30px"} margin={margin || "0 10px"} />
 
+const DeleteIcon: React.FunctionComponent<ImageProps & Styleable> = 
+    ({ width, height, margin, onClick, style }) => <SvgIcon style={{ cursor: 'pointer', ...style }} onClick={onClick} src={deleteicon} width={width || '50px'} height={height || "30px"} margin={margin || "0 10px"} />
+
+const CheckIcon: React.FunctionComponent<ImageProps & Styleable> =
+    ({ width, height, margin, onClick, style }) => <SvgIcon onClick={onClick} style={{ cursor: 'pointer', ...style }} src={checkicon} width={width || '50px'} height={height || "30px"} margin={margin || "0 10px"} />
+
+const CancelIcon: React.FunctionComponent<ImageProps & Styleable> =
+    ({ width, height, margin, onClick, style }) => <SvgIcon onClick={onClick} style={{ cursor: 'pointer', ...style }} src={cancelicon} width={width || '50px'} height={height || "30px"} margin={margin || "0 10px"} />
+
 export {
     SvgIcon as AppLogo, PlainAppLogo, MyLinksIcon, InboxIcon, OutboxIcon, RequestSentIcon, ViewBtnIcon,
-    InvitesIcon, UserListAvatar, PendingIcon, BtnAddLinksIcon, AddLinksIcon, CloseIcon, DocumentSvg
+    InvitesIcon, UserListAvatar, PendingIcon, BtnAddLinksIcon, AddLinksIcon, CloseIcon, DocumentSvg, DeleteIcon,
+    CheckIcon, CancelIcon
 }
