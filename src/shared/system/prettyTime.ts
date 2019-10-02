52dd6f1a7140b313269e2e2a2514e43d88148b03
Chris Sperry <christophermartinsperry@gmail.com>
2019-10-02 13:54:43 +0100

    generalize prettyTime

--- src/shared/system/prettyTime.ts
+++ src/shared/system/prettyTime.ts
@@ -1,17 +1,40 @@
-export const prettyTime = (secs: number) => {
-  let s = secs | 0;
-  const seconds = s % 60;
-  s = (s / 60) | 0;
-  const minutes = s % 60;
-  s = (s / 60) | 0;
-  const hours = s % 24;
-  s = (s / 24) | 0;
-  const days = s;
-  const segs = [
-    days > 0 ? `${days}d` : null,
-    hours > 0 ? `${hours}h` : null,
-    minutes > 0 ? `${minutes}m` : null,
-    seconds > 0 ? `${seconds}s` : null
+import { blinq } from "blinq";
+
+type TimeInterval = "s" | "m" | "h" | "d" | "wk";
+interface IntervalDescription {
+  label: TimeInterval;
+  interval: number | null;
+}
+
+export const prettyTime = (() => {
+  const intervals: IntervalDescription[] = [
+    { label: "s", interval: 60 },
+    { label: "m", interval: 60 },
+    { label: "h", interval: 24 },
+    { label: "d", interval: 7 },
+    { label: "wk", interval: null }
   ];
-  return segs.filter(s => s != null).join(" ");
-};
+  const bIntervals = blinq(intervals);
+
+  const pt = (secs: number) => {
+    const isNegative = secs < 0;
+    secs = Math.abs(secs);
+    const agg = bIntervals.aggregate(
+      { parts: [] as (string | null)[], rem: secs },
+      (acc, curr) => {
+        const v = curr.interval ? (acc.rem | 0) % curr.interval : acc.rem | 0;
+        const vs = v > 0 ? `${v}${curr.label}` : null;
+        const r = acc.rem / (curr.interval == null ? 1 : curr.interval);
+        return { parts: [...acc.parts, vs], rem: r };
+      }
+    );
+    const parts = blinq(agg.parts)
+      .reverse()
+      .where(s => s != null)
+      .toArray();
+    const absPretty = parts.length === 0 ? "0s" : parts.join(" ");
+
+    return `${isNegative ? "-" : ""}${absPretty}`;
+  };
+  return pt;
+})();
