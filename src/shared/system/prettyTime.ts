70f880e65f4fa59976b1e3c0dbd331bbe7ed82a2
Chris Sperry <christophermartinsperry@gmail.com>
2019-10-02 12:50:05 +0100

    add days interval to prettyTime

--- src/shared/system/prettyTime.ts
+++ src/shared/system/prettyTime.ts
@@ -1,14 +1,17 @@
-export const prettyTime = (secs: number) => {
-    let s = secs | 0;
-    const seconds = s % 60;
-    s = (s / 60) | 0;
-    const minutes = s % 60;
-    s = (s / 60) | 0;
-    const hours = s;
-    const segs = [
-        hours > 0 ? `${hours}h` : null,
-        minutes > 0 ? `${minutes}m` : null,
-        seconds > 0 ? `${seconds}s` : null
-    ];
-    return segs.filter(s => s != null).join(" ");
+const prettyTime = (secs: number) => {
+  let s = secs | 0;
+  const seconds = s % 60;
+  s = (s / 60) | 0;
+  const minutes = s % 60;
+  s = (s / 60) | 0;
+  const hours = s % 24;
+  s = (s / 24) | 0;
+  const days = s;
+  const segs = [
+    days > 0 ? `${days}d` : null,
+    hours > 0 ? `${hours}h` : null,
+    minutes > 0 ? `${minutes}m` : null,
+    seconds > 0 ? `${seconds}s` : null
+  ];
+  return segs.filter(s => s != null).join(" ");
 };
