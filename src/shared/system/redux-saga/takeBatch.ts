65e0c5fdf4d716510d358c53c0c686ab42afd962
Chris Sperry <christophermartinsperry@gmail.com>
2019-10-05 18:22:56 +0100

    fix typo

--- src/shared/system/redux-saga/takeBatch.ts
+++ src/shared/system/redux-saga/takeBatch.ts
@@ -8,26 +8,34 @@ interface TakeBatchOptions {
 const defaultTakeBatchOptions: TakeBatchOptions = {
     minDurationMs: 1000,
 };
-const neverPromise = new Promise<void>(() => { });
+const neverPromise = new Promise<void>(() => {});
 const neverEffect = call(() => neverPromise);
-export const takeBatch = <T>(patOrChan: Pattern | Channel<T>, opts: Partial<TakeBatchOptions> = {}) => {
+export const takeBatch = <T>(
+    patOrChan: Pattern | Channel<T>,
+    opts: Partial<TakeBatchOptions> = {}
+) => {
     const { minDurationMs, maxDurationMs, maxSize } = {
         ...defaultTakeBatchOptions,
         ...opts,
     };
-    return call(function* () {
+    return call(function*() {
         const receivedEvts: T[] = [];
         const evt: T = yield take(patOrChan as any);
-        const batchMaxDelay = maxDurationMs != null ? delay(maxDurationMs) : neverEffect;
+        const batchMaxDelay =
+            maxDurationMs != null ? delay(maxDurationMs) : neverEffect;
         receivedEvts.push(evt);
-        for (; ;) {
+        for (;;) {
             const takeFx = take(patOrChan as any);
             type RaceResult = {
                 evt: T;
                 nextMessageTimeout: unknown;
                 maxTimeout: unknown;
             };
-            const { evt, nextMessageTimeout, maxTimeout, }: RaceResult = yield race({
+            const {
+                evt,
+                nextMessageTimeout,
+                maxTimeout,
+            }: RaceResult = yield race({
                 evt: takeFx,
                 nextMessageTimeout: delay(minDurationMs),
                 maxTimeout: batchMaxDelay,
@@ -35,9 +43,11 @@ export const takeBatch = <T>(patOrChan: Pattern | Channel<T>, opts: Partial<Take
             if (evt) {
                 receivedEvts.push(evt);
             }
-            if (nextMessageTimeout ||
-                (maxSize != null && receivedEvts.length > maxSize) ||
-                maxTimeout) {
+            if (
+                nextMessageTimeout ||
+                (maxSize != null && receivedEvts.length >= maxSize) ||
+                maxTimeout
+            ) {
                 console.log(`sending batch`, {
                     evt: !!evt,
                     nextMessageTimeout: !!nextMessageTimeout,
